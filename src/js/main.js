12df8f99971a2da6854e9b3362a4845a18d562fe
Alex <almiab1@epsg.upv.es>
2021-08-06 20:48:14 +0200

    smooth scroll and icons added

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,56 @@
 // Toggle navbar
-document.addEventListener('DOMContentLoaded', () => {
+const burger = document.getElementById('navbar-burger');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active');
+  document.getElementById(burger.dataset.target).classList.toggle('is-active');
+});
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
+const navItems = document.getElementById('menubar').firstElementChild.children,
+  navSections = new Array(navItems.length);
+
+for (i = 0; i < navItems.length; i++)
+  navSections[i] = document.getElementById(navItems[i].dataset.target);
+
+const menuBarHeight = document.getElementById('menubar').offsetHeight;
+function isVisible(ele) {
+  const r = ele.getBoundingClientRect();
+  const h = (window.innerHeight || document.documentElement.clientHeight);
+  const w = (window.innerWidth || document.documentElement.clientWidth);
+  return (r.top <= h) &&
+    (r.top + r.height - menuBarHeight >= 0) &&
+    (r.left <= h) &&
+    (r.left + r.width >= 0);
+}
+
+function activateIfVisible() {
+  for (b = true, i = 0; i < navItems.length; i++) {
+    if (b && isVisible(navSections[i])) {
+      navItems[i].classList.add('is-active');
+      b = false;
+    } else
+      navItems[i].classList.remove('is-active');
+  }
+}
+
+var isTicking = null;
+window.addEventListener('scroll', () => {
+  if (!isTicking) {
+    window.requestAnimationFrame(() => {
+      activateIfVisible();
+      isTicking = false;
+    });
+    isTicking = true;
+  }
+}, false);
+
+for (item of navItems) {
+  item.addEventListener('click', e => {
+    e.preventDefault();
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: document.getElementById(e.target.dataset.target).getBoundingClientRect().top +
+        window.scrollY
+    });
   });
+}
