fec068d2e79c65631dd939cbebb121366f24398d
Alex <almiab1@epsg.upv.es>
2021-08-11 19:49:08 +0200

    baseland projects with basic estructure and examples

--- src/js/main.js
+++ src/js/main.js
@@ -62,3 +62,42 @@ for (item of navItems) {
     });
   });
 }
+
+// ------------------------------------------------------
+// Poryects Grid
+// ------------------------------------------------------
+
+// init Isotope
+var iso = new Isotope('.grid', {
+  itemSelector: '.grid-item',
+  resize: true,
+  masonry: {
+      // set to the element
+      isFitWidth: true
+  }
+});
+
+// Get filter buttons click
+var sortByGroup = document.querySelector('.sort-by-button-group');
+
+// Filter event
+var filter = function(event) {
+  // only <a> clicks
+  if (!matchesSelector(event.target, 'a')) {
+      return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // Filter
+  iso.arrange({
+      filter: filterValue
+  });
+
+  // Switch is-active effect
+  var current = document.getElementsByClassName("sort-by-button-group")[0].getElementsByClassName("is-active")[0];
+  current.className = "";
+
+  event.target.parentNode.className = "is-active"
+}
+
+// Add event
+sortByGroup.addEventListener('click', filter);
